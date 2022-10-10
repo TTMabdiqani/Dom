d554525b399ec3b540fd3712db57f203b256b922
MMTabdiqani <abdiqani.omar1@gmail.com>
2022-10-09 23:40:26 -0700

    basic DOM

---
+++ main.js
@@ -0,0 +1,12 @@
+let elements = document.querySelectorAll('.heading-1');
+// h.innerText = "Hello World!";
+
+for(let element of elements){
+    element.classList = "text-white text-2xl bg-blue-300 text-center";
+    
+}
+
+
+function hundler(){
+    console.log('hello world!');
+}
