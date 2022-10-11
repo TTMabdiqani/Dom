97c26e8d67cca890f6b597e64947b589d101f4e1
MMTabdiqani <abdiqani.omar1@gmail.com>
2022-10-11 14:54:32 +0300

    button click event handler

--- main.js
+++ main.js
@@ -6,7 +6,47 @@ for(let element of elements){
     
 }
 
-
-function hundler(){
-    console.log('hello world!');
+//event handler
+function clickHandler(){
+  const button = document.querySelector('button');
+  button.classList.add('text-white', 'bg-red-400', 'rounded', 'shadow-lg');
+button.onclick = clickHandlerAgain;
 }
+
+function clickHandlerAgain(){
+  alert("other click handler is worked!");
+
+}
+
+function sayHello(){
+  // console.log('Hello Friends!');
+
+  let buttons = document.querySelectorAll('.btn');
+  for(let button of buttons){
+  button.classList.add('text-2xl');
+}
+
+}
+
+function sayHi(){
+  // console.log('Hi Friends');
+
+let buttons = document.querySelector('.btn');
+buttons.classList.add('text-2xl');
+
+
+}
+
+let buttons = document.querySelectorAll('.btn');
+
+for(const button of buttons){
+
+  button.addEventListener('click', sayHello);
+  button.addEventListener('click', sayHi);
+  button.addEventListener('click',function(){
+    console.log('i great you!');
+  })
+  
+  button.removeEventListener('click',sayHi);
+}
+
